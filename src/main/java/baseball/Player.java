426dbd8648ce058ed3cc21be9bf2dad50e11626a
jaehyuuk <doogun97@gmail.com>
2023-10-31 16:35:33 +0900

    feat : 사용자 입력 기능 구현
    사용자 입력 값 타입 변환 및 에러 체크 기능 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -8,50 +8,52 @@ import java.util.List;
 import java.util.Set;
 
 public class Player {
-    public static final String MAXIMUM_NUMBER_3_EXCEPTION = "3개의 숫자만 입력하세요.";
-    public static final String DUPLICATED_EXCEPTION = "중복되지 않은 숫자만 입력하세요.";
-    public static final String WRONG_INPUT_EXCEPTION = "1(재시작) 또는 2(종료)를 입력하세요.";
+    private static final String ONLY_THREE_ERROR_MESSAGE = "3개의 숫자만 입력하세요.";
+    private static final String DUPLICATED_ERROR_MESSAGE = "중복되지 않은 숫자만 입력하세요.";
+    private static final String NUMBER_ERROR_MESSAGE = "숫자가 아닌 값은 입력할 수 없습니다.";
+    private static final String WRONG_INPUT_ERROR_MESSAGE = "1(재시작) 또는 2(종료)를 입력하세요.";
 
     public List<Integer> setPlayerNumber() throws IllegalArgumentException {
         String playerInput = Console.readLine();
         return playerInputToInteger(playerInput);
     }
 
-    public List<Integer> playerInputToInteger(String playerInput) throws IllegalArgumentException {
+    private List<Integer> playerInputToInteger(String playerInput) throws IllegalArgumentException {
         String[] split = playerInput.split("");
         if (split.length != 3) {
-            throw new IllegalArgumentException(MAXIMUM_NUMBER_3_EXCEPTION);
+            throw new IllegalArgumentException(ONLY_THREE_ERROR_MESSAGE);
         }
-
         List<Integer> numbers = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             numbers.add(Integer.parseInt(split[i]));
         }
-
         checkDuplicatedNumber(numbers);
         return numbers;
     }
 
-    public void checkDuplicatedNumber(List<Integer> numbers) {
-        Set<Integer> notDuple = new HashSet<>();
+    private void checkDuplicatedNumber(List<Integer> numbers) {
+        Set<Integer> checkNumber = new HashSet<>();
         for (int num : numbers) {
-            if (notDuple.contains(num)) {
-                throw new IllegalArgumentException(DUPLICATED_EXCEPTION);
+            if (checkNumber.contains(num)) {
+                throw new IllegalArgumentException(DUPLICATED_ERROR_MESSAGE);
             }
-            notDuple.add(num);
+            checkNumber.add(num);
         }
     }
 
     public boolean restart() {
         String num = Console.readLine();
-        return numToBoolean(num);
+        return checkNumberException(num);
     }
 
-    public boolean numToBoolean(String num) {
-        int number = Integer.parseInt(num);
-        if (number == 1) return true;
-        if (number == 2) return false;
-        throw new IllegalArgumentException(WRONG_INPUT_EXCEPTION);
+    private boolean checkNumberException(String num) {
+        try {
+            int number = Integer.parseInt(num);
+            if (number == 1) return true;
+            if (number == 2) return false;
+            throw new IllegalArgumentException(WRONG_INPUT_ERROR_MESSAGE);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
+        }
     }
-
 }
